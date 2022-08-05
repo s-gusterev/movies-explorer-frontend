b15180f6655402c7203e207433603b712ba8c2f4
s-gusterev <copywriter-sg@yandex.ru>
2022-08-05 14:12:43 +0300

    сверстал страницу с карточками

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './MoviesCardList.css';
+import MoviesCard from '../MoviesCard/MoviesCard';
+import { movieList } from '../../utils/variables';
 
 const MoviesCardList = () => {
-  return <ul className='movies-list'></ul>;
+  return (
+    <ul className='movies-list'>
+      {movieList.map((card, index) => (
+        <MoviesCard
+          key={index}
+          link={card.image}
+          title={card.title}
+          time={card.time}
+          like={card.like}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default MoviesCardList;
--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,3 +1,16 @@
+import img1 from '../images/movies-poster/1.jpeg';
+import img2 from '../images/movies-poster/2.jpeg';
+import img3 from '../images/movies-poster/3.jpeg';
+import img4 from '../images/movies-poster/4.jpeg';
+import img5 from '../images/movies-poster/5.jpeg';
+import img6 from '../images/movies-poster/6.jpeg';
+import img7 from '../images/movies-poster/7.jpeg';
+import img8 from '../images/movies-poster/8.jpeg';
+import img9 from '../images/movies-poster/9.jpeg';
+import img10 from '../images/movies-poster/10.png';
+import img11 from '../images/movies-poster/11.jpeg';
+import img12 from '../images/movies-poster/12.jpeg';
+
 const listTechs = [
   'HTML',
   'CSS',
@@ -23,4 +36,79 @@ const linksPortfolio = [
   },
 ];
 
-export { listTechs, linksPortfolio };
+const movieList = [
+  {
+    image: img1,
+    title: '33 слова о дизайне',
+    time: '1ч 47м',
+    like: true,
+  },
+  {
+    image: img2,
+    title: 'Киноальманах «100 лет дизайна»',
+    time: '1ч 3м',
+    like: false,
+  },
+  {
+    image: img3,
+    title: 'В погоне за Бенкси',
+    time: '1ч 42м',
+    like: true,
+  },
+  {
+    image: img4,
+    title: 'Баския: Взрыв реальности',
+    time: '1ч 21м',
+    like: false,
+  },
+  {
+    image: img5,
+    title: 'Бег это свобода',
+    time: '1ч 44м',
+    like: false,
+  },
+  {
+    image: img6,
+    title: 'Книготорговцы',
+    time: '1ч 37м',
+    like: true,
+  },
+  {
+    image: img7,
+    title: 'Когда я думаю о Германии ночью',
+    time: '1ч 56м',
+    like: false,
+  },
+  {
+    image: img8,
+    title: 'Gimme Danger: История Игги и The Stooge...',
+    time: '1ч 59м',
+    like: false,
+  },
+  {
+    image: img9,
+    title: 'Дженис: Маленькая девочка грустит',
+    time: '1ч 42м',
+    like: true,
+  },
+  {
+    image: img10,
+    title: 'Соберись перед прыжком',
+    time: '1ч 10м',
+    like: true,
+  },
+  {
+    image: img11,
+    title: 'Пи Джей Харви: A dog called money',
+    time: '1ч 4м',
+    like: false,
+  },
+  {
+    image: img12,
+    title: 'По волнам: Искусство звука в кино',
+    time: '1ч 7м',
+    like: false,
+  },
+];
+
+export { listTechs, linksPortfolio, movieList };
