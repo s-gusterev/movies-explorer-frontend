8f28a5459a582ca266cc6fbc8bfc98c08c7a4bb6
s-gusterev <copywriter-sg@yandex.ru>
2022-08-06 13:48:30 +0300

    настроил базовый роутинг

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Routes, Route, useRoutes } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
@@ -9,21 +10,43 @@ import Profile from '../Profile/Profile';
 import Registr from '../Registr/Registr';
 import Login from '../Login/Login';
 import NotFound from '../NotFound/NotFound';
+
 const App = () => {
+  const HeaderRoutes = () => {
+    return useRoutes([
+      { path: '/', element: <Header /> },
+      { path: '/movies', element: <Header /> },
+      { path: '/saved-movies', element: <Header /> },
+      { path: '/profile', element: <Header /> },
+    ]);
+  };
+  const FooterRoutes = () => {
+    return useRoutes([
+      { path: '/', element: <Footer /> },
+      { path: '/movies', element: <Footer /> },
+      { path: '/saved-movies', element: <Footer /> },
+      { path: '/profile', element: <Footer /> },
+    ]);
+  };
+
   return (
     <React.Fragment>
-      {/* <Header /> */}
+      <HeaderRoutes />
       <main className='main'>
-        {/* <Main /> */}
-        {/* <Movies /> */}
-        {/* <SavedMovies /> */}
-        {/* <Profile name='Виталий' email='pochta@yandex.ru' /> */}
-        {/* <Registr /> */}
-        {/* <Login /> */}
-        <NotFound />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/movies' element={<Movies />} />
+          <Route path='/saved-movies' element={<SavedMovies />} />
+          <Route
+            path='/profile'
+            element={<Profile name='Виталий' email='pochta@yandex.ru' />}
+          />
+          <Route path='/signup' element={<Registr />} />
+          <Route path='/signin' element={<Login />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
       </main>
-
-      {/* <Footer /> */}
+      <FooterRoutes />
     </React.Fragment>
   );
 };
